578842f35f51c9e8930687d6e631e6c22bb741b9
pulok <mdmunifhasan8520@gmail.com>
2019-03-09 16:50:42 +0600

    Initial Commit

---
+++ StoryGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryGame
+//
+//  Created by MacBook Pro on 3/9/19.
+//  Copyright © 2019 Code Drizzlers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
